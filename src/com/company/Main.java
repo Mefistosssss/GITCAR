f47d4bdcc4e4e5615f7f64b0a1be0f237126a1a3
Mefistosssss <oleksandrkondaurovit@gmail.com>
2022-01-26 20:18:11 -0800

    here is your car now dont ask me about changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,17 @@ public class Main {
     public static void main(String[] args) {
 	// the basic structure
         CarBasics car= new CarBasics();
+<<<<<<< Updated upstream
         car.color="red";
         car.engine="6543fdd";
         car.door="2";
         car.number="213123";
+=======
+        car.color="green";
+        car.number="789";
+        car.engine="asd131";
+        car.door="3";
+>>>>>>> Stashed changes
         System.out.println(car.color);
     }
 }

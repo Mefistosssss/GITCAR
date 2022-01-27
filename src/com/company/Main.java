fe04b27c2c4d58ce59d3147bb8822f738a91cb13
Mefistosssss <oleksandrkondaurovit@gmail.com>
2022-01-26 19:57:22 -0800

    the red car is ready for the trial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// the basic structure
         CarBasics car= new CarBasics();
-        
+        car.color="red";
+        car.engine="6543fdd";
+        car.door="2";
+        car.number="213123";
+        System.out.println(car.color);
     }
 }
